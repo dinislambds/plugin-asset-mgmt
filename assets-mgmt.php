8cd62a93e349bc4c1f9220d8548f76465f275f49
dinislambds <din2243@gmail.com>
2018-10-13 20:04:21 +0600

    initial commit

---
+++ assets-mgmt.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Plugin Assets Management
+Plugin URI: https://github.com/dinislambds/
+Description: Plugin Assets Management is just a assets management procedures as a learning purpose of plugin development course.
+Version: 1.0
+Author: Md Din Islam
+Author URI: https://dinislambds.com/
+License: GPLv2 or later
+Text Domain: assets-mgmt
+Domain Path: /languages/
+*/
+
+
+?>
